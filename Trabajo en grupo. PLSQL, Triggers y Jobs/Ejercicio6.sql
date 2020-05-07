1243ea6d8cf5f6b147b1f66d9d3b707670520f1f
David-Florido <davidfloridoruiz@gmail.com>
2020-05-07 14:08:12 +0200

    Ejercicio 6

--- Trabajo en grupo. PLSQL, Triggers y Jobs/Ejercicio6.sql
+++ Trabajo en grupo. PLSQL, Triggers y Jobs/Ejercicio6.sql
@@ -24,41 +24,54 @@ END Gestion_descuentos;
 
 ALTER SESSION SET NLS_DATE_FORMAT = 'DD-MM-YYYY';   --Asegurarse de usar formato YYYYY para los años
 
-CREATE OR REPLACE PROCEDURE P_Calcular_Descuento(cliente VARCHAR2, fecha_actual NUMBER) AS
+CREATE OR REPLACE PROCEDURE P_Calcular_Descuento(cliente VARCHAR2, fecha_actual_yyyy NUMBER) AS
 n_servicios NUMBER;
 n_mas_cinco_dias NUMBER;
 n_esperas_mas_de_la_media NUMBER;
 n_descuentos_total NUMBER;
-fecha_validez NUMBER;
+fecha_validez_yyyy NUMBER;
 BEGIN
     SELECT COUNT(IDFACTURA) INTO n_servicios FROM FACTURA WHERE CLIENTE_IDCLIENTE = cliente;
     SELECT COUNT(IDCITA) INTO n_mas_cinco_dias FROM CITA WHERE (FECHA_CONCERTADA - FECHA_SOLICITUD > 5 AND CLIENTE_IDCLIENTE = cliente);
     SELECT ROUND(SUM(FECREALIZACION - FECAPERTURA)/COUNT(IDSERVICIO)) INTO n_esperas_mas_de_la_media FROM SERVICIO
-        JOIN VEHICULO ON (SERVICIO.VEHICULO_NUMBASTIDOR = VEHICULO.NUMBASTIDOR ) 
-        JOIN CLIENTE ON (VEHICULO.CLIENTE_IDCLIENTE = CLIENTE.IDCLIENTE);
+    JOIN VEHICULO ON (SERVICIO.VEHICULO_NUMBASTIDOR = VEHICULO.NUMBASTIDOR )
+    JOIN CLIENTE ON (VEHICULO.CLIENTE_IDCLIENTE = CLIENTE.IDCLIENTE);
     n_descuentos_total := n_servicios + n_mas_cinco_dias + n_esperas_mas_de_la_media;
-    fecha_validez := fecha_actual + 1;
+    fecha_validez_yyyy := fecha_actual_yyyy + 1;
     IF n_descuentos_total > 10 THEN
-        INSERT INTO fidelizacion(descuento, cliente_idcliente, fecha_yyyy)  VALUES (10, cliente, fecha_validez);
+        UPDATE fidelizacion
+            SET descuento         = 10,
+                cliente_idcliente = cliente,
+                fecha_yyyy        = fecha_validez_yyyy
+            WHERE cliente_idcliente = cliente;
     ELSE
-        INSERT INTO fidelizacion(descuento, cliente_idcliente, fecha_yyyy)  VALUES (n_descuentos_total, cliente, fecha_validez);
+        UPDATE fidelizacion
+            SET descuento         = n_descuentos_total,
+                cliente_idcliente = cliente,
+                fecha_yyyy        = fecha_validez_yyyy
+            WHERE cliente_idcliente = cliente;
     END IF;
 END P_Calcular_Descuento;
 /
 
-
 /*2. El procedimiento P_Aplicar_descuento tomará el año y el cliente. Si en la tabla FIDELIZACIÓN hay un descuento calculado a aplicar ese año,
     lo hará para todas las facturas que encuentre (en ese año).
     */
-CREATE OR REPLACE PROCEDURE P_Aplicar_descuento(cliente VARCHAR2, fecha_yyyy NUMBER) AS
-tiene_descuento NUMBER;
+    
+CREATE OR REPLACE PROCEDURE P_Aplicar_descuento(cliente VARCHAR2, yyyy NUMBER) AS
+var_descuento NUMBER;
+CURSOR c_facturas IS (SELECT idfactura, descuento, total FROM factura WHERE cliente_idcliente = cliente AND fecemision LIKE ('%'||TO_CHAR(yyyy))) FOR UPDATE;
 BEGIN
-    SELECT COUNT(cliente_idcliente) INTO tiene_descuento FROM fidelizado;
-    IF tiene_descuento > 0 THEN
-        --Realizar actualizacion
-    END IF;
-    COMMIT;
-END;
+    SELECT descuento INTO var_descuento FROM fidelizacion WHERE cliente_idcliente = cliente AND fecha_yyyy = yyyy;
+    FOR v_cursor IN c_facturas LOOP
+    UPDATE factura SET
+        descuento = var_descuento,
+        total = (var_descuento/100) * total
+    WHERE
+        idfactura = v_cursor.idfactura;
+    END LOOP;
+    EXCEPTION
+        WHEN no_data_found THEN
+            dbms_output.put_line  ('Cliente sin descuentos');
 END P_Aplicar_descuento;
 /
-
