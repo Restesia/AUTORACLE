08020df16096302d334e09018d7576221443e7e9
David-Florido <davidfloridoruiz@gmail.com>
2020-05-21 16:30:33 +0200

    Correccion Ejercicio 3

--- Trabajo en grupo. PLSQL, Triggers y Jobs/Ejercicio3.sql
+++ Trabajo en grupo. PLSQL, Triggers y Jobs/Ejercicio3.sql
@@ -8,20 +8,28 @@ ALTER TABLE factura  ADD (
     total NUMBER
 );
 
-CREATE OR REPLACE PROCEDURE P_CALCULA_FACT(id_factura NUMBER) AS
-res_iva_calculado NUMBER;
-res_total NUMBER;
--- Hacer un select en vez de todo el cursor
-CURSOR c_res IS SELECT SUM(preciounidadventa * iva/100) iva_calculado, SUM(preciounidadventa + preciounidadventa * iva/100) total FROM factura 
-    JOIN contiene ON (factura.idfactura = contiene.factura_idfactura) 
-    JOIN pieza ON (contiene.pieza_codref = pieza.codref) WHERE factura.idfactura = id_factura;
+CREATE OR REPLACE PROCEDURE p_calcula_fact (id_factura NUMBER) AS
+    res_iva_calculado   NUMBER;
+    res_total           NUMBER;
 BEGIN
-    OPEN c_res;
-        FETCH c_res INTO res_iva_calculado, res_total;
-        UPDATE factura
-            SET iva_calculado = res_iva_calculado,
-                total         = res_total
-        WHERE idfactura = id_factura;
-    CLOSE c_res;
-END P_CALCULA_FACT;
+    SELECT
+        SUM(preciounidadventa * iva / 100) iva_calculado,
+        SUM(preciounidadventa + preciounidadventa * iva / 100) total
+    INTO
+        res_iva_calculado,res_total
+    FROM
+        factura
+        JOIN contiene ON ( factura.idfactura = contiene.factura_idfactura )
+        JOIN pieza ON ( contiene.pieza_codref = pieza.codref )
+    WHERE
+        factura.idfactura = id_factura;
+
+    UPDATE factura
+        SET
+            iva_calculado = res_iva_calculado,
+            total = res_total
+    WHERE
+        idfactura = id_factura;
+        
+END p_calcula_fact;
 /
