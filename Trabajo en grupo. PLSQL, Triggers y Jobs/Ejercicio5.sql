db5ebdc54b228bdc3477e2067ec5c91ef2ad50c9
alejandrofuster <9201111+AlejandroFuster@users.noreply.github.com>
2020-05-24 01:43:25 +0200

    Ejercicio 5 funcionando

--- Trabajo en grupo. PLSQL, Triggers y Jobs/Ejercicio5.sql
+++ Trabajo en grupo. PLSQL, Triggers y Jobs/Ejercicio5.sql
@@ -4,10 +4,12 @@ SET SERVEROUTPUT ON
 
 -- Creamos el paquete desde autoracle:
 CREATE OR REPLACE PACKAGE AUTORACLE_ANALISIS IS
-    --¿Tipos como object o record?
+    -- Definimos el tipo de datos que retiene la media, minimo y maximo de la consulta
     TYPE T_MEDIA_MIN_MAX IS RECORD(MEDIA NUMBER, MINIMO NUMBER, MAXIMO NUMBER);
+    -- Definimos el tipo de datos que retiene el numero de dias y horas
     TYPE T_TIEMPOS IS RECORD (DIAS NUMBER, HORAS NUMBER);
     
+    -- Definimos las funciones y el procedimiento
     FUNCTION  F_CALCULAR_PIEZAS(COD IN VARCHAR2, ANYO in VARCHAR2) RETURN T_MEDIA_MIN_MAX;
     FUNCTION  F_CALCULAR_TIEMPOS RETURN T_TIEMPOS;
     PROCEDURE P_RECOMPENSA;
@@ -23,13 +25,17 @@ CREATE OR REPLACE PACKAGE BODY AUTORACLE_ANALISIS AS
 FUNCTION F_CALCULAR_PIEZAS (COD IN VARCHAR2, ANYO IN VARCHAR2) RETURN T_MEDIA_MIN_MAX AS
     resultado T_MEDIA_MIN_MAX;
 BEGIN
+    -- Realizamos la consulta
     SELECT AVG(LOTE."NÚMERO_DE_PIEZAS"),MIN(LOTE."NÚMERO_DE_PIEZAS"),MAX(LOTE."NÚMERO_DE_PIEZAS") 
     INTO resultado 
     FROM COMPRA 
     INNER JOIN LOTE
     ON COMPRA.IDCOMPRA = LOTE.COMPRA_IDCOMPRA
     WHERE EXTRACT(YEAR FROM FECEMISION) = ANYO AND PIEZA_CODREF = COD;
-    DBMS_OUTPUT.PUT_LINE('MIN'||resultado.minimo);
+    -- Mostramos los valores
+    DBMS_OUTPUT.PUT_LINE('MIN '||resultado.minimo);
+    DBMS_OUTPUT.PUT_LINE('MAX '||resultado.maximo);
+    DBMS_OUTPUT.PUT_LINE('MEAN '||resultado.media);
     RETURN resultado;
 END F_CALCULAR_PIEZAS;
 
@@ -41,24 +47,71 @@ END F_CALCULAR_PIEZAS;
 FUNCTION F_CALCULAR_TIEMPOS RETURN T_TIEMPOS AS
     resultado T_TIEMPOS;
 BEGIN
+        -- Realizamos la consulta
         SELECT (SUM((s.FECREALIZACION - s.FECRECEPCION)) / COUNT(*)), SUM(HORAS)/COUNT(*)
         INTO resultado
         FROM servicio s
         INNER JOIN reparacion r ON s.IDSERVICIO = r.IDSERVICIO
+        -- Evitamos las fechas que estan vacias
         WHERE FECREALIZACION IS NOT NULL AND FECRECEPCION IS NOT NULL;
+        -- Mostramos los valores
+        DBMS_OUTPUT.PUT_LINE('DIAS '||resultado.dias);
+        DBMS_OUTPUT.PUT_LINE('HORAS '||resultado.horas);
         RETURN resultado;
 END F_CALCULAR_TIEMPOS;
 
 -- TODO: ULTIMO APARTADO
----- 3. El procedimiento P_Recompensaencuentra el servicio proporcionado
+---- 3. El procedimiento P_Recompensa encuentra el servicio proporcionado
 ---- más rápido y más lento(en días) y a los empleados involucrados los 
 ---- recompensa con un +/-5% en su sueldo base respectivamente...
 PROCEDURE P_RECOMPENSA AS
- idIncrementa NUMBER;
- idDecrementa NUMBER;
+
+    -- Valores para guardar los id de los empleados
+    idIncrementa NUMBER;
+    idDecrementa NUMBER;
+    
+    -- Inicializamos las variables para comparar
+    mas_rapido NUMBER := POWER(2,418)-1;
+    mas_lento NUMBER := 0;
+    
+    -- Cursor sobre el que iterar con los servicios, empleados y fechas
+     CURSOR cur IS
+     SELECT s.IDSERVICIO as id_servicio,
+       t.EMPLEADO_IDEMPLEADO as id_empleado,
+       ((s.FECREALIZACION - s.FECRECEPCION)) as dias
+    FROM AUTORACLE.SERVICIO s
+        JOIN AUTORACLE.TRABAJA t ON s.IDSERVICIO = t.SERVICIO_IDSERVICIO
+        WHERE FECREALIZACION IS NOT NULL AND FECRECEPCION IS NOT NULL;
+ 
 BEGIN
-        DBMS_OUTPUT.PUT_LINE(''); 
+        for r in cur loop
+        -- Buscamos el mas rapido
+            IF r.dias < mas_rapido THEN
+                idIncrementa := r.id_empleado;
+                mas_rapido := r.dias;
+            END IF;
+        -- Buscamos el mas lento
+            IF r.dias > mas_lento THEN
+                idDecrementa := r.id_empleado;
+                mas_lento := r.dias;
+            END IF;
+        END LOOP;
+        
+        DBMS_OUTPUT.PUT_LINE('Incrementar ' || idIncrementa || ' por servicio de  ' || mas_rapido);
+        DBMS_OUTPUT.PUT_LINE('Decrementar ' || idDecrementa || ' por servicio de  ' || mas_lento);
+
+        -- Aplicamos la penalizacion sobre el lento
+        UPDATE AUTORACLE.EMPLEADO
+            SET SUELDOBASE = SUELDOBASE - (0.05 * SUELDOBASE)
+            WHERE IDEMPLEADO = idDecrementa;
+        
+        -- Aplicamos la recompensa sobre el rapido
+        UPDATE AUTORACLE.EMPLEADO
+            SET SUELDOBASE = SUELDOBASE + (0.05 * SUELDOBASE)
+            WHERE IDEMPLEADO = idIncrementa;
+            
+        COMMIT;            
+        
 END P_RECOMPENSA;
 
 END;
-/
