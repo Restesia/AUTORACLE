2b70dbcb5933a1aeb96358203edf7e1bfc2cc99c
alejandrofuster <9201111+AlejandroFuster@users.noreply.github.com>
2020-05-25 00:37:26 +0200

    Arreglado job

--- Trabajo en grupo. PLSQL, Triggers y Jobs/Ejercicio9.sql
+++ Trabajo en grupo. PLSQL, Triggers y Jobs/Ejercicio9.sql
@@ -11,18 +11,18 @@ BEGIN
         auto_drop => FALSE,
         comments => 'Llamada al procedimiento P_REVISA Diariamente a las 21:00');
 END;
-
+/
 -- Crear otro JOB que anualmente (el 31 de diciembre a las 23.55) llame a P_Recompensa
 
 BEGIN
     DBMS_SCHEDULER.CREATE_JOB (
         job_name => 'Llamada_P_Recompensa',
         job_type => 'STORED_PROCEDURE',
-        job_action => 'AUTORACLE.P_Recompensa',
+        job_action => 'AUTORACLE_ANALISIS.P_RECOMPENSA',
         start_date => TO_DATE('2020-12-31 23:55:00' , 'YYYY-MM-DD HH24:MI:SS'),
         repeat_interval => 'FREQ = YEARLY; INTERVAL=1',
         enabled => TRUE,
         auto_drop => FALSE,
         comments => 'Llamada al procedimiento P_Recompensa anualmente, cada 31 de Diciembre a las 23:55');
 END;
-
+/
