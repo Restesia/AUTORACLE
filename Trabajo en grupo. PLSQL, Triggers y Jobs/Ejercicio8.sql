e3193cc164872bde8605d361f008a119cc309a74
alejandrofuster <9201111+AlejandroFuster@users.noreply.github.com>
2020-05-25 00:19:38 +0200

    Pruebas trigger

--- Trabajo en grupo. PLSQL, Triggers y Jobs/Ejercicio8.sql
+++ Trabajo en grupo. PLSQL, Triggers y Jobs/Ejercicio8.sql
@@ -5,6 +5,30 @@
 CREATE OR REPLACE TRIGGER TR_ELIMINA_CLIENTE_FIDELIZADO
 BEFORE DELETE ON CLIENTE FOR EACH ROW
 BEGIN
-	DELETE FROM FIDELIZACION WHERE CLIENTE = :old.IDCLIENTE;
+	DELETE FROM FIDELIZACION WHERE CLIENTE_IDCLIENTE = :old.IDCLIENTE;
 	DELETE FROM VEHICULO WHERE CLIENTE_IDCLIENTE = :old.IDCLIENTE;
 END TR_ELIMINA_CLIENTE_FIDELIZADO;
+/
+
+-- PRUEBA
+
+--- Creamos un cliente
+select * from cliente;
+
+insert into cliente values (1924,81281057,'Marcello','Mastroianni',NULL,'marcello@fellini.com',NULL);
+
+--- Le asignamos un vehiculo
+insert into vehiculo values (040807,'0008MEZ',NULL,22,3,1924,8);
+
+--- Lo fidelizamos
+
+insert into fidelizacion values(1,1924,2020);
+
+-- Borramos
+delete from cliente where idcliente = 1924;
+
+-- Comprobamos la eliminacion
+select * from fidelizacion where CLIENTE_IDCLIENTE = 1924;
+select * from vehiculo where CLIENTE_IDCLIENTE = 1924;
+select * from cliente where IDCLIENTE = 1924;
+
