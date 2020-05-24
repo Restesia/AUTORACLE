d8b1e0149e716201265da25ef143fcfc61ed381a
alejandrofuster <9201111+AlejandroFuster@users.noreply.github.com>
2020-05-24 02:05:02 +0200

    Cambios en los triggers

--- Trabajo en grupo. PLSQL, Triggers y Jobs/Ejercicio9.sql
+++ Trabajo en grupo. PLSQL, Triggers y Jobs/Ejercicio9.sql
@@ -1,22 +1,28 @@
+-- Crear un JOB que ejecute el procedimiento P_REVISA todos los díasa las 21:00.
+
 BEGIN
     DBMS_SCHEDULER.CREATE_JOB (
         job_name => 'Llamada_P_Revisa',
-        job_type => 'PLSQL_BLOCK',
-        job_action => 'BEGIN PROCEDURE P_REVISA END;',
+        job_type => 'STORED_PROCEDURE',
+        job_action => 'AUTORACLE.P_REVISA',
         start_date => SYSDATE,
-        repeat_interval => 'FREQ = DAILY;BYHOUR=21',
+        repeat_interval => 'FREQ = DAILY; BYHOUR=21',
         enabled => TRUE,
-        comments => 'Llamada al procedimiento P_Recompensa anualmente, cada 31 de Diciembre a las 23:55');
+        auto_drop => FALSE,
+        comments => 'Llamada al procedimiento P_REVISA Diariamente a las 21:00');
 END;
+
+-- Crear otro JOB que anualmente (el 31 de diciembre a las 23.55) llame a P_Recompensa
 
 BEGIN
     DBMS_SCHEDULER.CREATE_JOB (
         job_name => 'Llamada_P_Recompensa',
-        job_type => 'PLSQL_BLOCK',
-        job_action => 'BEGIN PROCEDURE P_Recompensa END;',
+        job_type => 'STORED_PROCEDURE',
+        job_action => 'AUTORACLE.P_Recompensa',
         start_date => TO_DATE('2020-12-31 23:55:00' , 'YYYY-MM-DD HH24:MI:SS'),
         repeat_interval => 'FREQ = YEARLY; INTERVAL=1',
         enabled => TRUE,
+        auto_drop => FALSE,
         comments => 'Llamada al procedimiento P_Recompensa anualmente, cada 31 de Diciembre a las 23:55');
 END;
 
