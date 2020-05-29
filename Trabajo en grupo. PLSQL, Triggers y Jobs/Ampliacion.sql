a3185139ee4c2b89cf49f3ed8a1203c8237727e4
alejandrofuster <9201111+AlejandroFuster@users.noreply.github.com>
2020-05-29 14:10:14 +0200

    Trigger puertas

---
+++ Trabajo en grupo. PLSQL, Triggers y Jobs/Ampliacion.sql
@@ -0,0 +1,19 @@
+-- Trigger para limitar el numero de puertas entre 3 o 5
+CREATE OR REPLACE TRIGGER TR_PUERTAS_VEHICULO
+BEFORE INSERT OR UPDATE ON MODELO FOR EACH ROW
+BEGIN
+    IF (:new.NUMPUERTAS != 3 AND :new.NUMPUERTAS != 5) THEN
+        raise_application_error(-20001,'Numero de puertas distinto de 3 o 5');
+    END IF;
+END TR_PUERTAS_VEHICULO;
+/
+
+-- Prueba
+
+SELECT * FROM MARCA;
+
+    -- No funciona por tener 6 puertas
+insert into modelo values(20,5,'M73',6,NULL,NULL);
+    -- Funciona
+insert into modelo values(20,5,'M73',5,NULL,NULL);
+
